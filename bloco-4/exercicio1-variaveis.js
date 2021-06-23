766415215b546475f43d162cc3b9ae28b159d20b
Seu nome <mgabiss10@gmail.com>
2021-06-23 15:22:46 -0300

    Atribuiçao de variaveis

---
+++ bloco-4/exercicio1-variaveis.js
@@ -0,0 +1,12 @@
+const name = "Maria Gabriela";
+const birthCity = "São Paulo";
+let birthYear = 1997;
+
+birthYear = 2030;
+birthCity = "Minas Gerais";
+
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);
+
+/* birthCity deu erro pois sua variável declarada é uma constante
