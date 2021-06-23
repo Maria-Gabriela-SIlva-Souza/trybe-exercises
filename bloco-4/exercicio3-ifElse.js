ab44d8806ae72a597950233416052c949bc6b7f4
Seu nome <mgabiss10@gmail.com>
2021-06-23 16:11:40 -0300

    Realizado exercicio 3

---
+++ bloco-4/exercicio3-ifElse.js
@@ -0,0 +1,11 @@
+let candidatx = 85;
+
+if (candidatx >= 80) {
+    console.log("Parabéns, você foi aprovadx!")
+}
+else if (candidatx < 80 && candidatx >= 60) {
+console.log("Você está na nossa lista de espera")
+} 
+else {
+    console.log ("Você foi reprovadx")
+}
