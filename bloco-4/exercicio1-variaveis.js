2e9981ca3fdf7f1ca8dd1f55dde0701fc1021049
Seu nome <mgabiss10@gmail.com>
2021-06-23 15:44:07 -0300

    Exercicio typeof feito

--- bloco-4/exercicio1-variaveis.js
+++ bloco-4/exercicio1-variaveis.js
@@ -9,4 +9,11 @@ console.log(name);
 console.log(birthCity);
 console.log(birthYear);
 
-/* birthCity deu erro pois sua variável declarada é uma constante
+/* Crie uma constante chamada name e atribua a ela o seu nome (Exemplo: Carolina);
+Crie uma constante chamada birthCity e atribua a ela a sua cidade natal;
+Crie uma variável chamada birthYear e atribua a ela o ano em que você nasceu;
+Utilize o console.log() para imprimir as constantes e variáveis que você criou;
+Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece!
+
+Altere o valor atribuído à constante birthCity . Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro? 🤔
+-- birthCity deu erro pois sua variável declarada é uma constante
