58e6fdb7753a565ced7d394c221f6cc8f947835c
Seu nome <mgabiss10@gmail.com>
2021-06-24 15:29:04 -0300

    Adiçao de uma linha sobre cada comando

--- bloco-4/exercise/introducao-a-javascript-e-logica-de-programacao_2/exercicio4-videoAula.js
+++ bloco-4/exercise/introducao-a-javascript-e-logica-de-programacao_2/exercicio4-videoAula.js
@@ -1,5 +1,6 @@
 let numero = 7;
 
-for(let contador = 1; contador <= 9; contador ++){
-    console.log (numero * contador)
+for(let contador = 1; contador <= 9; contador ++) {
+    console.log ("");
+    console.log (numero * contador);
 }
