c4eb2206d23c10cb3ce5c9fe93f1606f486a9d72
Seu nome <mgabiss10@gmail.com>
2021-06-24 20:57:16 -0300

    Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado

--- bloco-4/exercise/introducao-a-javascript-e-logica-de-programacao_2/Exercicio6.js
+++ bloco-4/exercise/introducao-a-javascript-e-logica-de-programacao_2/Exercicio6.js
@@ -1,10 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0;
 
-for (let index = 0; index > numbers.length; index = + 1){
-    if (index === (2%))
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] % 2 !== 0) {
+    result += 1;
+  }
 }
 
-
-let ok = 3 % 3;
-
-console.log (ok);
+if (result === 0) {
+  console.log('nenhum valor ímpar encontrado');
+} else {
+  console.log(result);
+}
