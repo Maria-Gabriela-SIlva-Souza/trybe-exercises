c9b986ec99148fb7cdc3670ce9f4033b8baf273a
Seu nome <mgabiss10@gmail.com>
2021-06-24 18:35:29 -0300

    Exercicio 3 - Calcule e imprima a média aritmética dos valores contidos no array

--- bloco-4/exercise/introducao-a-javascript-e-logica-de-programacao_2/Exercicio3.js
+++ bloco-4/exercise/introducao-a-javascript-e-logica-de-programacao_2/Exercicio3.js
@@ -2,5 +2,4 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 let soma = numbers.reduce((acumulado, atual) => acumulado + atual, 0);
 
-
-console.log (soma);
+console.log (soma / numbers.length);
