9c712ce3b236b3d6da5a3c67eda07f4bddd28bda
Seu nome <mgabiss10@gmail.com>
2021-06-24 16:11:48 -0300

    Move arquivos para pasta 4.2

--- bloco-4/exercise/introducao-a-javascript-e-logica-de-programacao_2/exercicio4-VideoAulaForArray.js
+++
@@ -1,6 +0,0 @@
-let listaDeNomes = ['Joana', 'Maria', 'Lucas'];
-
-for (let indice = 0; indice < listaDeNomes.length; indice++){
-    let mensagem = 'Boas vindas,' + listaDeNomes[indice] + '!';
-    console.log (mensagem);
-}
