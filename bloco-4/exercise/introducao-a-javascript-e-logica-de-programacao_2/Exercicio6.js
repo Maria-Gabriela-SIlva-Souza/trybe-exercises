20e298b673ee4cd012b9319605fabb4be9d46353
Seu nome <mgabiss10@gmail.com>
2021-06-24 20:45:26 -0300

    Utilizando for , descubra qual o maior valor contido no array e imprima-o

---
+++ bloco-4/exercise/introducao-a-javascript-e-logica-de-programacao_2/Exercicio6.js
@@ -0,0 +1,10 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 0; index > numbers.length; index = + 1){
+    if (index === (2%))
+}
+
+
+let ok = 3 % 3;
+
+console.log (ok);
