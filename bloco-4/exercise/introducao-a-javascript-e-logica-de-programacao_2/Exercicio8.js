519e8b7bd08e8bc27c9c60864413768913b6a413
Seu nome <mgabiss10@gmail.com>
2021-06-24 21:10:01 -0300

    Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado

---
+++ bloco-4/exercise/introducao-a-javascript-e-logica-de-programacao_2/Exercicio8.js
@@ -0,0 +1,9 @@
+/* Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;*/
+
+let contagem = [];
+
+for (let index = 1; index <= 25; index += 1){
+        contagem.push(index);
+}
+
+console.log (contagem);
