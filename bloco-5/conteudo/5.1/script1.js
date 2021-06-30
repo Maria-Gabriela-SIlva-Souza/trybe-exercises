f9d49ff34ce4a77e4653dc2e6ae1c38518cbb8a8
Seu nome <mgabiss10@gmail.com>
2021-06-30 15:48:58 -0300

    Exercicio getElementsByClassName e getElementsByTagName

--- bloco-5/conteudo/5.1/script1.js
+++ bloco-5/conteudo/5.1/script1.js
@@ -1,5 +1,18 @@
+//Ex 1
+
 document.getElementById ("page-title").innerText = "Harry Potter";
 
 document.getElementById ("second-paragraph").innerText = "Melhor filme";
 
 document.getElementById ("subtitle").innerText = "CONSEGUI IUHUUUUUU";
+
+//Ex 2
+
+document.getElementsByClassName ("ex2");
+
+const paragraph = document.getElementById("paragraph");
+    paragraph.style.color = "red";
+
+const ex2 = document.getElementsByTagName('h4')[0];
+    ex2.style.font = 'bold';
+
