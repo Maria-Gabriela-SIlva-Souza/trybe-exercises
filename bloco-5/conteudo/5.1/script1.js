bc9e1fe6302955e6235490b149175ad7b91fd9b4
Seu nome <mgabiss10@gmail.com>
2021-06-30 15:18:14 -0300

    Exercicio getElementById

---
+++ bloco-5/conteudo/5.1/script1.js
@@ -0,0 +1,5 @@
+document.getElementById ("page-title").innerText = "Harry Potter";
+
+document.getElementById ("second-paragraph").innerText = "Melhor filme";
+
+document.getElementById ("subtitle").innerText = "CONSEGUI IUHUUUUUU";
