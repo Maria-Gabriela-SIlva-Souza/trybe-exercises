dd93ee0f61f94d848665c2913f22ab450bb1518c
Seu nome <mgabiss10@gmail.com>
2021-07-06 09:52:32 -0300

    Adiciona exercicio aula 5.1

--- bloco-5/conteudo/5.1/script1.js
+++ bloco-5/conteudo/5.1/script1.js
@@ -13,6 +13,5 @@ document.getElementsByClassName ("ex2");
 const paragraph = document.getElementById("paragraph");
     paragraph.style.color = "red";
 
-const ex2 = document.getElementsByTagName('h4')[0];
-    ex2.style.font = 'bold';
+document.getElementsByTagName('h4').style.color = "red";
 
