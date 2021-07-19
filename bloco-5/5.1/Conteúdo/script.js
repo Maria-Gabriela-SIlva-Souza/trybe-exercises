2df4e31c5476416ddd94311f4301484e449755a9
Seu nome <mgabiss10@gmail.com>
2021-07-19 16:11:10 -0300

    Organização das pastas

---
+++ bloco-5/5.1/Conteúdo/script.js
@@ -0,0 +1,34 @@
+//Ex 1
+
+document.getElementById ("page-title").innerText = "Harry Potter";
+
+document.getElementById ("second-paragraph").innerText = "Melhor filme";
+
+document.getElementById ("subtitle").innerText = "CONSEGUI IUHUUUUUU";
+
+//Ex 2
+
+document.getElementsByClassName ("ex2");
+
+const paragraph = document.getElementById("paragraph");
+    paragraph.style.color = "red";
+
+document.getElementsByTagName('h4').style.color = "red";
+
+//Ex 3
+
+document.querySelector ("#page-title").innerText = "Harry Potter";
+
+document.querySelector ("#second-paragraph").innerText = "Melhor filme";
+
+document.querySelector ("#subtitle").innerText = "CONSEGUI IUHUUUUUU";
+
+
+document.querySelectorAll (".ex2");
+
+    const paragraph = document.querySelector ("#paragraph");
+    paragraph.style.color = "red";
+
+document.querySelector('h4').style.color = "red";
+
+
