8e084f653122f8c856e9de913d5c7d1e2040aa9d
Seu nome <mgabiss10@gmail.com>
2021-07-21 09:51:33 -0300

    Ex 2 e 3

--- bloco-5/5.3/conteudo/main.js
+++ bloco-5/5.3/conteudo/main.js
@@ -22,13 +22,20 @@ secondLi.addEventListener('click', handleChangeTech);
 thirdLi.addEventListener('click', handleChangeTech);
 
 /*3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-com a classe 'tech';
+com a classe 'tech';*/
+input.addEventListener('input', function(event) {
+  const techElement = document.querySelector('.tech');
+  techElement.innerText = event.target.value;
+});
 
-4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+/*4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 redirecione para alguma página;
-4.1. Que tal redirecionar para seu portifólio?
+4.1. Que tal redirecionar para seu portifólio?*/
+myWebpage.addEventListener('dblclick', function() {
+  window.location.replace('https://blog.betrybe.com/');
+});
 
-5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+/*   5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 a cor do mesmo;
 
 Segue abaixo um exemplo do uso de event.target:
