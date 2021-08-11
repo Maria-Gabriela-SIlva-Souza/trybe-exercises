ae694900e062e182fef4d8acfaa69854eb9d2ed5
Seu nome <mgabiss10@gmail.com>
2021-08-11 15:51:56 -0300

    Consideraçoes finais sobre exercicios

--- bloco5_JavaScript_DOM_Eventos_e_Web_Storage/5.3_JavaScript-Eventos/main.js
+++ bloco5_JavaScript_DOM_Eventos_e_Web_Storage/5.3_JavaScript-Eventos/main.js
@@ -38,3 +38,29 @@ myWebpage.addEventListener ('dblclick', function () {
   window.location.replace('https://blog.betrybe.com/'); // a ação disparada é pegar a URL atual com window.location e substituir por outro link usando .replace('https://blog.betrybe.com/') .
 });
 
+/*4. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+a cor do mesmo;*/
+myWebpage.addEventListener ('mouseover', function(){
+  myWebpage.style.color = 'orange';
+});
+
+myWebpage.addEventListener('mouseout', function(event) {
+  event.target.style.color = 'unset';
+}); //adiciona o valor "unset" para a cor da fonte. Isso significa que esse estilo irá "resetar" a cor do elemento quando o mouse sair dele.
+
+
+
+/*Segue abaixo um exemplo do uso de event.target:*/
+
+function resetText(event) {
+  // O Event é passado como um parâmetro para a função.
+  event.target.innerText = 'Opção reiniciada';
+  // O event possui várias propriedades, porém a mais usada é o event.target,
+  // que retorna o objeto que disparou o evento.
+}
+
+firstLi.addEventListener('dblclick', resetText);
+
+// Não precisa passar o parâmetro dentro da callback resetText. O próprio
+// navegador fará esse trabalho por você, não é legal? Desse jeito, o
+// event.target na nossa função retornará o objeto 'firstLi'.
