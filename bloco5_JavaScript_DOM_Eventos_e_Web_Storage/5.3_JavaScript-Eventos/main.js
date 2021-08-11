f9b487688690622fc174688d540ecd3d3be13d1c
Seu nome <mgabiss10@gmail.com>
2021-08-11 15:49:46 -0300

    Exercicio 2

--- bloco5_JavaScript_DOM_Eventos_e_Web_Storage/5.3_JavaScript-Eventos/main.js
+++ bloco5_JavaScript_DOM_Eventos_e_Web_Storage/5.3_JavaScript-Eventos/main.js
@@ -26,29 +26,9 @@ Isso funciona como se estivéssemos selecionando a Li em que queremos escrever o
 /*2. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 com a classe 'tech';*/
 
-
-/*3. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
-redirecione para alguma página;
-1. Que tal redirecionar para seu portifólio?*/
-
-
-/*4. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
-a cor do mesmo;*/
+input.addEventListener ('input', function(event) { //Cria evento no input, com o parametro input e uma função anônima
+  const classeTech = document.querySelector('.tech'); // declara constante que contenha classe tech
+  classeTech.innerText = event.target.value; //no elemento da classe tech, atribui no evento próprio o valor inserido na mesma
+});
 
 
-
-
-/*Segue abaixo um exemplo do uso de event.target:*/
-
-function resetText(event) {
-  // O Event é passado como um parâmetro para a função.
-  event.target.innerText = 'Opção reiniciada';
-  // O event possui várias propriedades, porém a mais usada é o event.target,
-  // que retorna o objeto que disparou o evento.
-}
-
-firstLi.addEventListener('dblclick', resetText);
-
-// Não precisa passar o parâmetro dentro da callback resetText. O próprio
-// navegador fará esse trabalho por você, não é legal? Desse jeito, o
-// event.target na nossa função retornará o objeto 'firstLi'.
