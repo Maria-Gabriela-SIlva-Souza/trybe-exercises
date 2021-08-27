323044e56cdcc6b3628fe70b9248b3a52233efd0
Seu nome <mgabiss10@gmail.com>
2021-08-27 20:10:20 -0300

    Adicionando exercicio 7.1 paret 2

--- bloco7_Introdução_a_JavaScript_ES6_e_Testes_Unitários/JavaScript_ES6-let_const_arrow_functions_e_template_literals/exParte2.js
+++ bloco7_Introdução_a_JavaScript_ES6_e_Testes_Unitários/JavaScript_ES6-let_const_arrow_functions_e_template_literals/exParte2.js
@@ -4,6 +4,21 @@
 // Bônus (opcional): tente fazer o mesmo exercício de forma recursiva . 
 // Spoiler: É possível resolver com uma linha usando ternary operator .
 
+const factorial = number => {
+    let result = 1;
+
+    for (let index = 2; index <= number; index += 1) {
+        result *= index;
+    }
+
+    return result;
+}
+
+console.log(factorial(5));
+
+//RECURSIVA
+// const factorial = number => number > 1 ? number * factorial(number - 1) : 1;
+// console.log(factorial(5));
 
 
 //Exercicio 2
@@ -11,13 +26,23 @@
 // Exemplo:
 // Copiar
 //       longestWord("Antônio foi no banheiro e não sabemos o que aconteceu") // retorna 'aconteceu'
+const longestWord = text => {
+    let wordArray = text.split(' ');
+    let maxLength = 0;
+    let result = '';
 
+    for (const word of wordArray) {
+        if (word.length > maxLength) {
+            maxLength = word.length;
+            result = word;
+        }
+    }
 
-//Exercicio 3
-// Crie uma página que contenha:
-// Um botão ao qual será associado um event listener ;
-// Uma variável clickCount no arquivo JavaScript que acumule o número de clicks no botão;
-// Um campo no HTML que vá atualizando a quantidade de clicks no botão conforme a variável clickCount é atualizada.
+    return result;
+}
+
+console.log(longestWord("Antonio foi no banheiro e não sabemos o que aconteceu"));
+
 
 
 //Exercicio 4
@@ -29,9 +54,37 @@
 // Parâmetro: "Bebeto"
 // Retorno: "Tryber Bebeto aqui!"
 // Um array com escopo global, que é o escopo do arquivo JS , nesse caso, contendo cinco strings com suas principais skills .
+
 // Função 2 : Escreva uma função que vai receber a string retornada da Função 1 como parâmetro. Essa função deve concatenar as skills do array global à string que foi passada para a Função 2 via parâmetro. Você deve ordenar os skills em ordem alfabética. Sua função deve retornar essa nova string .
 // Exemplo: "Tryber x aqui! Minhas cinco principais habilidades são:
 // JavaScript;
 // HTML; ... #goTrybe".
 
+const array = ["Android", "iOS", "Architecture", "Teach", "Run"];
 
+function buildSkillsPhrase (paramOne) {
+    const fun1 = paramInner => (
+      `Tryber ${paramInner} aqui!
+
+      Tudo bem?`
+    );
+
+    let result = `${fun1(paramOne)}
+
+    Minhas cinco principais habilidades são:`;
+
+    array.forEach((skill, index) =>
+    result = `${result}
+
+    - ${skill}`);
+
+    result = `
+{result}
+
+    #goTrybe
+    `;
+
+    return result;
+}
+
+console.log(buildSkillsPhrase("Lucas"));
