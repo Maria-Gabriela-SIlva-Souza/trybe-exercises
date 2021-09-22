cdf71dfb901a48d5ef6714a887894282fd92784b
Seu nome <mgabiss10@gmail.com>
2021-09-21 21:39:20 -0300

    Exercicios 8.1

--- bloco8_Higher_Order_Functions_do_JavaScript_ES6/8.1_JavaScript_ES6-Introdução_a_Higher_Order_Functions/exercise1.js
+++ bloco8_Higher_Order_Functions_do_JavaScript_ES6/8.1_JavaScript_ES6-Introdução_a_Higher_Order_Functions/exercise1.js
@@ -1,16 +1,16 @@
 // 1 - Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com .
 
 const newEmployees = (callback) => {
-    const employees = {
+    const employ = {
       id1: callback('Pedro Guerra'), 
       id2: callback('Luiza Drumond'), 
       id3: callback('Carla Paiva'), 
     }
-    return employees;
+    return employ;
   };
 
   function employees(param) {
-    const email = `{${param}@trybe.com}`;
+    const email = `{${param}, ${param}@trybe.com}`;
     return email;
   }
 
