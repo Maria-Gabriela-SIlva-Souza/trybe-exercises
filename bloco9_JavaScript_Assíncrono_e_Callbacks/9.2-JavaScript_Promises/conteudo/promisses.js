655c71bd7669b0759346eed01b344ce4ef74a51c
Seu nome <mgabiss10@gmail.com>
2021-09-29 20:50:46 -0300

    Adicionando conteudo pratico aula 9.2

---
+++ bloco9_JavaScript_Assíncrono_e_Callbacks/9.2-JavaScript_Promises/conteudo/promisses.js
@@ -0,0 +1,37 @@
+// Parte 1 - Mostrando o Erro
+
+// const promise = new Promise((resolve, reject) => {
+//   const number = Math.floor(Math.random() * 11);
+
+//   if (number <= 5) {
+//     return reject(console.log(`Que fracasso =( Nosso número foi ${number}`));
+//   }
+//   resolve(console.log(`Que sucesso =) nosso número foi ${number}`));
+// });
+// ---------------------------------------------------------------------------------------
+
+// Parte 2 - .then()
+// const promise = new Promise((resolve, reject) => {
+//   const number = Math.floor(Math.random() * 11);
+
+//   if (number <= 5) {
+//     return reject(console.log(`Que fracasso =( Nosso número foi ${number}`));
+//   }
+//   resolve(number);
+// })
+// .then(number => `Que sucesso =) nosso número foi ${number}`)
+// .then(msg => console.log(msg));
+// ---------------------------------------------------------------------------------------
+
+// Parte 3 - .cath()
+const promise = new Promise((resolve, reject) => {
+  const number = Math.floor(Math.random()* 11);
+
+  if (number <= 5) {
+    return reject(number);
+  }
+  resolve(number);
+})
+.then(number => `Que sucesso =) nosso número foi ${number}`)
+.then(msg => console.log(msg))
+.catch(number => console.log(`Que fracasso =( Nosso número foi ${number}`));
