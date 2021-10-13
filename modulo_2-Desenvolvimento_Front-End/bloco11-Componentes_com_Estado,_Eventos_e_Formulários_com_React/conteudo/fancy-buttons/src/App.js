0b53e34c28d92d9abef32c95f2a89892f3fd9707
Seu nome <mgabiss10@gmail.com>
2021-10-13 11:03:49 -0300

    Exercicio fixaçao constructor

--- modulo_2-Desenvolvimento_Front-End/bloco11-Componentes_com_Estado,_Eventos_e_Formulários_com_React/conteudo/fancy-buttons/src/App.js
+++ modulo_2-Desenvolvimento_Front-End/bloco11-Componentes_com_Estado,_Eventos_e_Formulários_com_React/conteudo/fancy-buttons/src/App.js
@@ -5,19 +5,34 @@ import './App.css';
 não é a maneira correta de se criar eventos
 em React! A função se refere ao componente,
 então deve ser parte de sua classe! */
-function handleClick() {
-  console.log('Clicou no botão!')
-}
 
-function handleClick2() {
-  console.log('Clicou no botão 2!')
-}
+// 3 - Declare dentro da classe do seu componente dos exercícios de fixação acima a função que lida com o evento que antes era lidado por uma função do lado de fora da classe!
+// 4 - Garanta acesso ao objeto this na função que você declarou.
 
-function handleClick3() {
-  console.log('Clicou no botão 3!')
-}
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.handleClick = this.handleClick.bind(this)
+    this.handleClick2 = this.handleClick2.bind(this)
+    this.handleClick3 = this.handleClick3.bind(this)
+
+  }
+
+  handleClick() {
+    console.log(this)
+    console.log('Clicou no botão !!')
+  }
+  
+  handleClick2() {
+    console.log(this)
+    console.log('Clicou no botão 2!')
+  }
+  
+  handleClick3() {
+    console.log(this)
+    console.log('Clicou no botão 3!')
+  }
   /* Repare que, diferentemente do HTML, no
   JSX você associa uma função a um evento
   passando a própria função entre chaves `{}` */
@@ -25,9 +40,9 @@ class App extends React.Component {
     // 2 - Faça com que sua aplicação exiba três botões lado a lado com textos diferentes. Cada botão clicado deve acionar um evento diferente, cada um escrevendo algo diferente no console do navegador via console.log() .
     return (
       <div>
-        <button onClick={handleClick}>Meu botão</button>
-        <button onClick={handleClick2}>Meu botão 2</button>
-        <button onClick={handleClick3}>Meu botão 3</button>
+        <button onClick={this.handleClick}>Meu botão</button>
+        <button onClick={this.handleClick2}>Meu botão 2</button>
+        <button onClick={this.handleClick3}>Meu botão 3</button>
       </div>
     )
 
