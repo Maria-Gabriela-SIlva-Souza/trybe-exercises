b3397c4648bf60c9667e8521ca2a142c62f3ee75
Seu nome <mgabiss10@gmail.com>
2021-11-03 13:42:23 -0300

    11.1 - Exercicio de fixaçao 2 (this e bind)

--- modulo_2-Desenvolvimento_Front-End/bloco_11_-_Componentes_com_Estado,_Eventos_e_Formulários_com_React/11.1_-_Componentes_com_estado_e_eventos/conteudo/fancy-buttons/src/App.js
+++ modulo_2-Desenvolvimento_Front-End/bloco_11_-_Componentes_com_Estado,_Eventos_e_Formulários_com_React/11.1_-_Componentes_com_estado_e_eventos/conteudo/fancy-buttons/src/App.js
@@ -1,25 +1,33 @@
 import './App.css';
 import React from 'react';
 
-function handleClick() {
-  console.log('Clicou no primeiro botão!')
-}
-
-function handleClickAgain() {
-  console.log('Clicou no segundo botão!')
-}
-
-function handleClickOneMore() {
-  console.log('Clicou no terceiro botão!')
-}
-
 class App extends React.Component {
+  constructor() {
+    super()
+
+    this.handleClick = this.handleClick.bind(this)
+    this.handleClickAgain = this.handleClickAgain.bind(this)
+    this.handleClickOneMore = this.handleClickOneMore.bind(this)
+  }
+
+  handleClick() {
+    console.log('Clicou no primeiro botão!')
+  }
+  
+  handleClickAgain() {
+    console.log('Clicou no segundo botão!')
+  }
+  
+  handleClickOneMore() {
+    console.log('Clicou no terceiro botão!')
+  }
+
   render() {
     return (
       <div>
-        <button onClick={handleClick}>Meu botão</button>
-        <button onClick={handleClickAgain}>Meu outro botão</button>
-        <button onClick={handleClickOneMore}>Mais um botão</button>
+        <button onClick={this.handleClick}>Meu botão</button>
+        <button onClick={this.handleClickAgain}>Meu outro botão</button>
+        <button onClick={this.handleClickOneMore}>Mais um botão</button>
       </div>
     )
   }
