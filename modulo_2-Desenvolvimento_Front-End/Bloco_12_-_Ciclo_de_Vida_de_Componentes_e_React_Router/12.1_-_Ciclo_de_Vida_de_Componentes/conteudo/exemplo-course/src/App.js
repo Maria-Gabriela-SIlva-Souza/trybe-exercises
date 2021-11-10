2a6cff4ddfd93cb19fc8c9017564c52c8935f6d9
Seu nome <mgabiss10@gmail.com>
2021-11-10 19:59:47 -0300

    12.1 - Exercicio com requisiçao de API

--- modulo_2-Desenvolvimento_Front-End/Bloco_12_-_Ciclo_de_Vida_de_Componentes_e_React_Router/12.1_-_Ciclo_de_Vida_de_Componentes/conteudo/exemplo-course/src/App.js
+++ modulo_2-Desenvolvimento_Front-End/Bloco_12_-_Ciclo_de_Vida_de_Componentes_e_React_Router/12.1_-_Ciclo_de_Vida_de_Componentes/conteudo/exemplo-course/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import DadJoke from './DadJoke';
 
 function App() {
   return (
     <div className="App">
-
+      <DadJoke />
     </div>
   );
 }
--- modulo_2-Desenvolvimento_Front-End/Bloco_12_-_Ciclo_de_Vida_de_Componentes_e_React_Router/12.1_-_Ciclo_de_Vida_de_Componentes/conteudo/exemplo-course/src/DadJoke.js
+++ modulo_2-Desenvolvimento_Front-End/Bloco_12_-_Ciclo_de_Vida_de_Componentes_e_React_Router/12.1_-_Ciclo_de_Vida_de_Componentes/conteudo/exemplo-course/src/DadJoke.js
@@ -15,12 +15,17 @@ class DadJoke extends React.Component {
   }
 
   async fetchJoke() {
-    const requestHeaders = { headers: { Accept: 'application/json' } }
-    const requestReturn = await fetch('https://icanhazdadjoke.com/', requestHeaders)
-    const requestObject = await requestReturn.json();
-    this.setState({
-      jokeObj: requestObject,
-    })
+    this.setState(
+      {loading: true}, //Primeiro parametro setState();
+      async () => {
+        const requestHeaders = { headers: { Accept: 'application/json' } }
+        const requestReturn = await fetch('https://icanhazdadjoke.com/', requestHeaders)
+        const requestObject = await requestReturn.json();
+        this.setState({
+          loading: false,
+          jokeObj: requestObject,
+        })
+      })
   }
 
   componentDidMount() {
@@ -29,7 +34,11 @@ class DadJoke extends React.Component {
 
   saveJoke() {
     //Salvando a piada no array de piadas existentes
+    this.setState(({ storedJokes, jokeObj }) => ({
+      storedJokes: [...storedJokes, jokeObj]
+    }))
 
+    this.fetchJoke();
   }
 
   renderJokeElement() {
@@ -44,7 +53,7 @@ class DadJoke extends React.Component {
   }
 
   render() {
-    const { storedJokes } = this.state;
+    const { storedJokes, loading } = this.state;
     const loadingElement = <span>Loading...</span>;
 
     return (
@@ -53,7 +62,7 @@ class DadJoke extends React.Component {
           {storedJokes.map(({ id, joke }) => (<p key={id}>{joke}</p>))}
         </span>
 
-        <span>RENDERIZAÇÃO CONDICIONAL</span>
+        <span>{loading ? loadingElement : this.renderJokeElement()}</span>
 
       </div>
     );
