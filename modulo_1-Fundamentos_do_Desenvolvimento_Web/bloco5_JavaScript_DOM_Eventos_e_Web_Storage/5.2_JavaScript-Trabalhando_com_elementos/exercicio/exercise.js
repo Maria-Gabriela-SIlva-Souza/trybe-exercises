b2fbfddffa01ac13fbd943a6c39c6535dd396281
Seu nome <mgabiss10@gmail.com>
2021-12-14 13:02:33 -0300

    Bloco 5.2 - Exercicios praticos - Extras

--- modulo_1-Fundamentos_do_Desenvolvimento_Web/bloco5_JavaScript_DOM_Eventos_e_Web_Storage/5.2_JavaScript-Trabalhando_com_elementos/exercicio/exercise.js
+++ modulo_1-Fundamentos_do_Desenvolvimento_Web/bloco5_JavaScript_DOM_Eventos_e_Web_Storage/5.2_JavaScript-Trabalhando_com_elementos/exercicio/exercise.js
@@ -1,7 +1,6 @@
 // 1. Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body ;
 const body = document.getElementsByTagName('body')[0];
 const createH1 = document.createElement('h1');
-createH1.className = 'title';
 createH1.innerText = 'Exercício 5.2 - JavaScript DOM';
 body.appendChild(createH1);
 
@@ -46,14 +45,35 @@ const createUl = document.createElement('ul');
 sectionRight.appendChild(createUl);
 const ul = document.getElementsByTagName('ul')[0];
 
-for(i = 1; i <= 10; i += 1) {
+for (i = 1; i <= 10; i += 1) {
     const createLi = document.createElement('li');
     createLi.innerHTML = i;
     ul.appendChild(createLi);
 }
 
 // 9. Adicione 3 tags h3 , todas sendo filhas do main criado no passo 2.
-for(i = 1; i <= 3; i += 1) {
+// 11. Adicione a classe description nas 3 tags h3 criadas;
+
+for (i = 1; i <= 3; i += 1) {
     const createH3 = document.createElement('h3');
+    createH3.className = 'description';
     main.appendChild(createH3);
 }
+
+
+// Agora que você criou muita coisa, vamos fazer algumas alterações e remoções:
+// 10. Adicione a classe title na tag h1 criada;
+createH1.className = 'title';
+
+// 12. Remova a section criado no passo 5 (aquele que possui a classe left-content ). Utilize a função .removeChild() ;
+main.removeChild(sectionLeft);
+
+// 13. Centralize a section criado no passo 6 (aquele que possui a classe right-content ). Dica: para centralizar, basta configurar o margin-right: auto da section ;
+sectionRight.style.marginRight='auto';
+
+// 14. Troque a cor de fundo do elemento pai da section criada no passo 3 (aquela que possui a classe center-content ) para a cor verde;
+main.style.backgroundColor = 'green';
+
+// 15. Remova os dois últimos elementos ( nove e dez ) da lista criada no passo 8.
+ul.lastChild.remove();
+ul.lastChild.remove();
