908879e58daa2606160fa65da8083e19efc0b64a
Seu nome <mgabiss10@gmail.com>
2021-12-23 15:27:55 -0300

    Bloco 7.1 - Adicionando exercicios praticos

--- modulo_1-Fundamentos_do_Desenvolvimento_Web/bloco7_Introdução_a_JavaScript_ES6_e_Testes_Unitários/7.1_JavaScript_ES6-let_const_arrow_functions_e_template_literals/exercise/exParteII-4.js
+++ modulo_1-Fundamentos_do_Desenvolvimento_Web/bloco7_Introdução_a_JavaScript_ES6_e_Testes_Unitários/7.1_JavaScript_ES6-let_const_arrow_functions_e_template_literals/exercise/exParteII-4.js
@@ -1,51 +1,3 @@
-//Exercicio 1
-// Crie uma função que receba um número e retorne seu fatorial.
-// Na matemática, o fatorial de um número não negativo N , com a notação N! , é o produto de todos os inteiros menores ou iguais a N . Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
-// Bônus (opcional): tente fazer o mesmo exercício de forma recursiva . 
-// Spoiler: É possível resolver com uma linha usando ternary operator .
-
-const factorial = number => {
-    let result = 1;
-
-    for (let index = 2; index <= number; index += 1) {
-        result *= index;
-    }
-
-    return result;
-}
-
-console.log(factorial(5));
-
-//RECURSIVA
-// const factorial = number => number > 1 ? number * factorial(number - 1) : 1;
-// console.log(factorial(5));
-
-
-//Exercicio 2
-// Crie uma função que receba uma frase e retorne qual a maior palavra.
-// Exemplo:
-// Copiar
-//       longestWord("Antônio foi no banheiro e não sabemos o que aconteceu") // retorna 'aconteceu'
-const longestWord = text => {
-    let wordArray = text.split(' ');
-    let maxLength = 0;
-    let result = '';
-
-    for (const word of wordArray) {
-        if (word.length > maxLength) {
-            maxLength = word.length;
-            result = word;
-        }
-    }
-
-    return result;
-}
-
-console.log(longestWord("Antonio foi no banheiro e não sabemos o que aconteceu"));
-
-
-
-//Exercicio 4
 // Crie um código JavaScript com a seguinte especificação:
 // Não se esqueça de usar template literals
 // Função 1 : Escreva uma função que vai receber uma string como parâmetro. Sua função deverá procurar pela letra x em uma string qualquer que você determinar e substituir pela string que você passou como parâmetro. Sua função deve retornar essa nova string .
@@ -54,7 +6,6 @@ console.log(longestWord("Antonio foi no banheiro e não sabemos o que aconteceu"
 // Parâmetro: "Bebeto"
 // Retorno: "Tryber Bebeto aqui!"
 // Um array com escopo global, que é o escopo do arquivo JS , nesse caso, contendo cinco strings com suas principais skills .
-
 // Função 2 : Escreva uma função que vai receber a string retornada da Função 1 como parâmetro. Essa função deve concatenar as skills do array global à string que foi passada para a Função 2 via parâmetro. Você deve ordenar os skills em ordem alfabética. Sua função deve retornar essa nova string .
 // Exemplo: "Tryber x aqui! Minhas cinco principais habilidades são:
 // JavaScript;
@@ -79,7 +30,7 @@ function buildSkillsPhrase (paramOne) {
     - ${skill}`);
 
     result = `
-{result}
+    ${result}
 
     #goTrybe
     `;
