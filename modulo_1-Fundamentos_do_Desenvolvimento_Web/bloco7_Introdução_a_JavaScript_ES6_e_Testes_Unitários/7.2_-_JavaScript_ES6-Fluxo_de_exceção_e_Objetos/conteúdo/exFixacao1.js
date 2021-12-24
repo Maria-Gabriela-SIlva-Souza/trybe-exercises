de67371f4b217d8e2463b068a8e2bc152585a4ba
Seu nome <mgabiss10@gmail.com>
2021-12-23 22:15:31 -0300

    Bloco 7.2 - Exercicios praticos

---
+++ modulo_1-Fundamentos_do_Desenvolvimento_Web/bloco7_Introdução_a_JavaScript_ES6_e_Testes_Unitários/7.2_-_JavaScript_ES6-Fluxo_de_exceção_e_Objetos/conteúdo/exFixacao1.js
@@ -0,0 +1,16 @@
+// Tente criar uma função que exiba as habilidades do objeto student . Cada habilidade deve ser exibida no formato "Nome da habilidade, Nível: valor da chave referente à habilidade". 
+
+const student2 = {
+  Html: 'Bom',
+  Css: 'Ótimo',
+  JavaScript: 'Ruim',
+  SoftSkills: 'Ótimo',
+  Git: 'Bom',
+};
+
+const listSkills = (student) => {
+  const arrayOfSkills = Object.keys(student);
+  for(index in arrayOfSkills){
+    console.log(`${arrayOfSkills[index]}, Nível: ${student[arrayOfSkills[index]]}`);
+  }
+};
