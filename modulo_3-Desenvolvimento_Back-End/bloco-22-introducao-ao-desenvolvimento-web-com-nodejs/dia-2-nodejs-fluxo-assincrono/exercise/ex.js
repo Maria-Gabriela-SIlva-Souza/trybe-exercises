c2a1210f50e56d93eca0554e162d9f93cc6b0e74
Seu nome <mgabiss10@gmail.com>
2022-03-14 21:21:19 -0300

    Bloco 22.2 - Exercicios de fixaçao - Realizando ajustes em todos as ramificaçoes do exercicio 4, onde centralizei a leitura do documento em uma funçao, e passei para as outras

--- modulo_3-Desenvolvimento_Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercise/ex.js
+++ modulo_3-Desenvolvimento_Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercise/ex.js
@@ -2,11 +2,19 @@ const fs = require('fs').promises;
 
 const nomeDoArquivo = 'simpsons.json';
 
+const readFileSimpsons = async () => {
+  const simpsonDoc = await
+    fs.readFile(nomeDoArquivo, 'utf8')
+    .then((data) => JSON.parse(data))
+    .catch('erro na requisição')
+
+  return simpsonDoc;
+}
+
 // Exercicio 4.1
 
 // const readSimpsonsData = () => {
-//   fs.readFile(nomeDoArquivo, 'utf8')
-//   .then((data) => JSON.parse(data))
+//   readFileSimpsons()
 //   .then((result) => result.map((d) => console.log(`${d.id} - ${d.name}`)))
 //   .catch((err) => {
 //     console.error(`Não foi possível ler o arquivo ${nomeDoArquivo}\n Erro: ${err}`);
@@ -19,9 +27,7 @@ const nomeDoArquivo = 'simpsons.json';
 // Exercicio 4.2
 
 // const readSimpsonsId = async (idSelected) => {
-//   const simpsonDoc = await
-//     fs.readFile(nomeDoArquivo, 'utf8')
-//     .then((data) => JSON.parse(data))
+//   const simpsonDoc = await readFileSimpsons()
 
 //   const chosenSimpson = simpsonDoc.find((simpson) => simpson.id === (idSelected).toString());
 
@@ -38,9 +44,7 @@ const nomeDoArquivo = 'simpsons.json';
 // Exercicio 4.3
 
 const readSimpsonsDeleteIds = async () => {
-  const simpsonDoc = await
-    fs.readFile(nomeDoArquivo, 'utf8')
-    .then((data) => JSON.parse(data))
+  const simpsonDoc = await readFileSimpsons()
 
   const filterSimpson = simpsonDoc.filter((simpson) => simpson.id != '6' && simpson.id != '10');
 
@@ -48,7 +52,7 @@ const readSimpsonsDeleteIds = async () => {
     throw new Error('Requisição não encontrada');
   }
 
-  return console.log(chosenSimpson);
+  return console.log(filterSimpson);
 }
 
 readSimpsonsDeleteIds();
