699119bf1fb657c34b9dee4814bcd10dbfc0a214
Seu nome <mgabiss10@gmail.com>
2022-03-14 22:05:35 -0300

    Bloco 22.2 - Exercicio de fixaçao 4.6 finalizado

--- modulo_3-Desenvolvimento_Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercise/ex.js
+++ modulo_3-Desenvolvimento_Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercise/ex.js
@@ -80,19 +80,40 @@ const readFileSimpsons = async () => {
 
 // Exercicio 4.5
 
-const addPerson = async () => {
-  const simpsonDoc = await readFileSimpsons()
+// const addPerson = async () => {
+//   const simpsonDoc = await readFileSimpsons()
 
-  const personNelson = simpsonDoc
-    .filter(simpson => ['1', '2', '3', '4'].includes(simpson.id) || simpson.id === '5');
+//   const personNelson = simpsonDoc
+//     .filter(simpson => ['1', '2', '3', '4'].includes(simpson.id) || simpson.id === 'Nelson Muntz');
 
-  await fs.writeFile('./simpsonFamily.json', JSON.stringify(personNelson))
-  .then(() => {
-    console.log('Arquivo salvo');
-  })
-  .catch(() => {
-    console.error('err');
-  });
+//   await fs.writeFile('./simpsonFamily.json', JSON.stringify(personNelson))
+//   .then(() => {
+//     console.log('Arquivo salvo');
+//   })
+//   .catch(() => {
+//     console.error('err');
+//   });
+// }
+
+// addPerson();
+
+
+// Exercicio 4.6
+
+// Importamos o módulo de promises do fs
+const fs = require('fs').promises;
+
+function replaceNelson () {
+  // Realizamos a leitura do arquivo
+  return fs.readFile('./simpsonsFamily.json', 'utf-8')
+    // Interpretamos o conteúdo como JSON
+    .then((fileContent) => JSON.parse(fileContent))
+    // Filtramos o array para remover o personagem Nelson
+    .then((simpsons) => simpsons.filter((simpson) => simpson.id !== '8'))
+    // Criamos um novo Array contendo a personagem Maggie
+    .then((simpsonsWithoutNelson) => simpsonsWithoutNelson
+      .concat([{ id: '8', name: 'Maggie Simpson' }]))
+    // Escrevemos o novo array no arquivo
+    .then((simpsonsWithMaggie) =>
+      fs.writeFile('./simpsonsFamily.json', JSON.stringify(simpsonsWithMaggie)));
 }
-
-addPerson();
