e1dbc72c967cb946a4aa17d20dbf0ac902700164
Seu nome <mgabiss10@gmail.com>
2022-03-14 21:09:50 -0300

    Bloco 22.2 - Exercicio de fixaçao 4.2 finalizado

--- modulo_3-Desenvolvimento_Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercise/ex.js
+++ modulo_3-Desenvolvimento_Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercise/ex.js
@@ -2,13 +2,34 @@ const fs = require('fs').promises;
 
 const nomeDoArquivo = 'simpsons.json';
 
-const readSimpsonsData = () => {
-  fs.readFile(nomeDoArquivo, 'utf8')
-  .then((data) => JSON.parse(data))
-  .then((result) => result.map((d) => console.log(`${d.id} - ${d.name}`)))
-  .catch((err) => {
-    console.error(`Não foi possível ler o arquivo ${nomeDoArquivo}\n Erro: ${err}`);
-  });
+// Exercicio 4.1
+
+// const readSimpsonsData = () => {
+//   fs.readFile(nomeDoArquivo, 'utf8')
+//   .then((data) => JSON.parse(data))
+//   .then((result) => result.map((d) => console.log(`${d.id} - ${d.name}`)))
+//   .catch((err) => {
+//     console.error(`Não foi possível ler o arquivo ${nomeDoArquivo}\n Erro: ${err}`);
+//   });
+// }
+
+// readSimpsonsData();
+
+
+// Exercicio 4.2
+
+const readSimpsonsId = async (idSelected) => {
+  const simpsonDoc = await
+    fs.readFile(nomeDoArquivo, 'utf8')
+    .then((data) => JSON.parse(data))
+
+  const chosenSimpson = simpsonDoc.find((simpson) => simpson.id === (idSelected).toString());
+
+  if (!chosenSimpson) {
+    throw new Error('id não encontrado');
+  }
+
+  return console.log(chosenSimpson);
 }
 
-readSimpsonsData();
+readSimpsonsId(1);
