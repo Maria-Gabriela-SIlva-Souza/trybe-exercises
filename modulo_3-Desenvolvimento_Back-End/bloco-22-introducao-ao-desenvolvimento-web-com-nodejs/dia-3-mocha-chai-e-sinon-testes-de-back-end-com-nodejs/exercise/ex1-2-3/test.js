a0e91648d3787b424538c621b0737a80cd81e9ae
Seu nome <mgabiss10@gmail.com>
2022-03-15 15:27:10 -0300

    Bloco 22.3 - Exercicio de fixaçao 3 feito

--- modulo_3-Desenvolvimento_Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/exercise/ex1-2-3/test.js
+++ modulo_3-Desenvolvimento_Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/exercise/ex1-2-3/test.js
@@ -3,6 +3,23 @@ const { expect } = require('chai');
 const positivoOuNegativo = require('./positivoOuNegativo');
 
 describe('Executa a função positivoOuNegativo', () => {
+
+  describe('quando o valor de input for diferente de número', () => {
+    describe('a resposta', () => {
+      it('é um erro', () => {
+        const resposta = positivoOuNegativo('string');
+
+        expect(resposta).not.be.a('number');
+      });
+
+      it('é a mensagem "o valor deve ser um número"', () => {
+        const resposta = positivoOuNegativo('string');
+
+        expect(resposta).to.be.equals('o valor deve ser um número');
+      });
+    });   
+  });
+
   describe('quando o número for maior que 0', () => {
     describe('a resposta', () => {
       it('é uma "string"', () => {
