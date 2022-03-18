a90d60dc45dbd99de8a1b62f5d62fc5e7ecb2131
Seu nome <mgabiss10@gmail.com>
2022-03-18 17:09:27 -0300

    Bloco 22.4 - Ajustando documento para realizaçao dos exercicios. - Exercicio 1 feito

---
+++ modulo_3-Desenvolvimento_Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercise/README.md
@@ -0,0 +1,4 @@
+1. Crie uma rota GET /ping
+  - Sua rota deve retornar o seguinte JSON: { message: 'pong' }
+
+
---
+++ modulo_3-Desenvolvimento_Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercise/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/ping', (_req, res) => {
+  res.status(200).json({ message: 'pong' });
+});
+
+app.use((err, _req, res, _next) => {
+  res.status(500).send(`Algo deu errado! Mensagem: ${err.message}`);
+});
+
+app.listen(3001, () => {
+  console.log('Aplicação ouvindo na porta 3001');
+});
+
