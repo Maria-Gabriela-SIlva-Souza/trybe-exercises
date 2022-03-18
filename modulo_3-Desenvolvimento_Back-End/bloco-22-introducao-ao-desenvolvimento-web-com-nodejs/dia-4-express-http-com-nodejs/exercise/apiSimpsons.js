4f913575ea45eefd84299ec027d1543ff0e309ea
Seu nome <mgabiss10@gmail.com>
2022-03-18 18:09:18 -0300

    Bloco 22.4 - Exercicio 7 feito

--- modulo_3-Desenvolvimento_Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercise/apiSimpsons.js
+++ modulo_3-Desenvolvimento_Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercise/apiSimpsons.js
@@ -4,10 +4,18 @@ const app = express();
 const simpsonsData = require('./readSimpsons')
 
 // Usando async/await pois a requisição de leitura provém de uma promise
-app.get('/simpsons', async(_req, res) => {
+app.get('/simpsons', async (_req, res) => {
   const simpsons = await simpsonsData.getSimpsons();
+    res.status(200).json(simpsons);
+});
 
-  res.status(200).json(simpsons);
+app.get('/simpsons/:id', async(req, res) => {
+  const simpsons = await simpsonsData.getSimpsons();
+  const { id } = req.params;
+  const simpsonsId = simpsons.find((s) => s.id === id);
+
+  if(!simpsonsId) { return res.status(404).json({ message: 'simpson not found' }) };
+  res.status(202).json(simpsonsId);
 });
 
 app.use((err, _req, res, _next) => {
