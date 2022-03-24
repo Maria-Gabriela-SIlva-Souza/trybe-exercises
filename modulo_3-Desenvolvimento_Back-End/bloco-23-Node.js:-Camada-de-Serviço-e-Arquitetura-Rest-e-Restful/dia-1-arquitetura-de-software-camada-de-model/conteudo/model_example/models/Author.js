a3cc7dffcd159faaad88f6126277ddd0045e0ec9
Seu nome <mgabiss10@gmail.com>
2022-03-24 16:00:11 -0300

    Bloco 23.1 - Exemplo course model_exemplo - adicionando Função realizada para concatenar o nome e trazer o nome completo

--- modulo_3-Desenvolvimento_Back-End/bloco-23-Node.js:-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-1-arquitetura-de-software-camada-de-model/conteudo/model_example/models/Author.js
+++ modulo_3-Desenvolvimento_Back-End/bloco-23-Node.js:-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-1-arquitetura-de-software-camada-de-model/conteudo/model_example/models/Author.js
@@ -2,6 +2,18 @@
 
 const connection = require('./connection');
 
+// Função realizada para concatenar o nome e trazer o nome completo
+const getNewAuthor = ({ id, firstName, middleName, lastName }) => {
+  const fullName = [ firstName, middleName, lastName ].filter((name) => name).join(' ');
+  return {
+    id,
+    firstName,
+    middleName,
+    lastName,
+    fullName
+  }
+}
+
 // função para ajustar a forma de saída das chaves para camelCase
 const serialize = ({ id, first_name, middle_name, last_name }) => {
   return {
@@ -16,7 +28,7 @@ const serialize = ({ id, first_name, middle_name, last_name }) => {
 const getAll = async () => {
   // Se não dessestruturarmos esse array a requisição vem com indormações do banco de dados além das pedidas
   const [ authors ] = await connection.execute('SELECT id, first_name, middle_name, last_name FROM authors');
-  return authors.map(serialize);
+  return authors.map(serialize).map(getNewAuthor);
 }
 
 module.exports = {
