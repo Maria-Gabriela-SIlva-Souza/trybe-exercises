6da9ce5daa5c2b21a459b9280761ae2ac29376fe
Seu nome <mgabiss10@gmail.com>
2022-03-24 17:59:41 -0300

    Bloco 23.1 - Exemplo course model_exemplo - organizando os middleares em pastas

--- modulo_3-Desenvolvimento_Back-End/bloco-23-Node.js:-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-1-arquitetura-de-software-camada-de-model/conteudo/model_example/index.js
+++ modulo_3-Desenvolvimento_Back-End/bloco-23-Node.js:-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-1-arquitetura-de-software-camada-de-model/conteudo/model_example/index.js
@@ -1,62 +1,15 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-const port = 3000
+const port = 3000;
 
-const Authors = require('./models/Author');
-const Books = require('./models/Books');
+const authorsRouter = require('./routers/authorsRouter');
+const booksRouter = require('./routers/booksRouter');
 
 app.use(bodyParser.json())
 
-app.get('/authors', async (_req, res) => {
-  const authors = await Authors.getAll();
+app.use('/authors', authorsRouter);
 
-  res.status(200).json(authors);
-});
-
-app.get('/authors/:id', async (req, res) => {
-  const { id } = req.params;
-
-  const author = await Authors.findById(id);
-
-  if(!author) return res.status(404).json({message: 'Not Found'});
-
-  res.status(200).json(author);
-});
-
-app.post('/authors', async (req, res) => {
-  const { first_name, middle_name, last_name } = req.body;
-
-  if(!Authors.isValid(first_name, middle_name, last_name)) return res.status(400).json({message: 'Dados inválidos'});
-
-  await Authors.create(first_name, middle_name, last_name);
-  
-  res.status(201).json({message: 'Author criado com sucesso'});
-});
-
-app.get('/books', async (_req, res) => {
-  const books = await Books.getAll();
-
-  res.status(200).json(books);
-});
-
-app.get('/books/:author_id', async (req, res) => {
-  const  { author_id } = req.params;
-  const booksByAuthorId = await Books.getByAuthorId(author_id);
-
-  if(!booksByAuthorId) return res.status(404).json({message: 'Not Found'});
-
-  res.status(200).json(booksByAuthorId);
-});
-
-app.post('/books', async (req, res) => {
-  const { title, author_id } = req.body;
-
-  if(!await Books.isValid(title, author_id)) return res.status(400).json({message: 'Dados inválidos'});
-
-  await Books.create(title, author_id);
-  
-  res.status(201).json({message: 'Livro criado com sucesso'});
-});
+app.use('/books', booksRouter);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
---
+++ modulo_3-Desenvolvimento_Back-End/bloco-23-Node.js:-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-1-arquitetura-de-software-camada-de-model/conteudo/model_example/routers/authorsRouter.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+
+const Authors = require('../models/Author');
+
+router.get('/', async (_req, res) => {
+  const authors = await Authors.getAll();
+
+  res.status(200).json(authors);
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const author = await Authors.findById(id);
+
+  if(!author) return res.status(404).json({message: 'Not Found'});
+
+  res.status(200).json(author);
+});
+
+router.post('/', async (req, res) => {
+  const { first_name, middle_name, last_name } = req.body;
+
+  if(!Authors.isValid(first_name, middle_name, last_name)) return res.status(400).json({message: 'Dados inválidos'});
+
+  await Authors.create(first_name, middle_name, last_name);
+  
+  res.status(201).json({message: 'Author criado com sucesso'});
+});
+
+module.exports = router;
