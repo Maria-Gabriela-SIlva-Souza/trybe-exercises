e055b27320972171c9843dbd7d68496c78b84d6f
Seu nome <mgabiss10@gmail.com>
2022-03-24 17:19:17 -0300

    Bloco 23.1 - Exemplo course model_exemplo - adicionando metodo POST para dicionar um author

--- modulo_3-Desenvolvimento_Back-End/bloco-23-Node.js:-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-1-arquitetura-de-software-camada-de-model/conteudo/model_example/index.js
+++ modulo_3-Desenvolvimento_Back-End/bloco-23-Node.js:-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-1-arquitetura-de-software-camada-de-model/conteudo/model_example/index.js
@@ -1,9 +1,12 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const port = 3000
 
 const Authors = require('./models/Author');
 const Books = require('./models/Books');
+
+app.use(bodyParser.json())
 
 app.get('/authors', async (_req, res) => {
   const authors = await Authors.getAll();
@@ -19,6 +22,16 @@ app.get('/authors/:id', async (req, res) => {
   if(!author) return res.status(404).json({message: 'Not Found'});
 
   res.status(200).json(author);
+});
+
+app.post('/authors', async (req, res) => {
+  const { firstName, middleName, lastName } = req.body;
+
+  if(!Authors.isValid(firstName, middleName, lastName)) return res.status(400).json({message: 'Dados inválidos'});
+
+  await Authors.create(firstName, middleName, lastName);
+  
+  res.status(201).json({message: 'Author criado com sucesso'});
 });
 
 app.get('/books', async (_req, res) => {
