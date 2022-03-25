dc236d0c7ed4612b4f49d034f86e0a7700326c21
Seu nome <mgabiss10@gmail.com>
2022-03-25 15:40:43 -0300

    Bloco 23.1 - Exercicio 1  de fixaçao => Finalizado

--- modulo_3-Desenvolvimento_Back-End/bloco-23-Node.js:-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-1-arquitetura-de-software-camada-de-model/exercise/users-manager/models/User.js
+++ modulo_3-Desenvolvimento_Back-End/bloco-23-Node.js:-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-1-arquitetura-de-software-camada-de-model/exercise/users-manager/models/User.js
@@ -4,7 +4,7 @@ const connection = require('./connection');
 // Faremos a função `formatUser`
 // Note que não realizamos destructuring da senha, pois não precisaremos dela
 // Note, também, que renomeamos os campos fist_name e last_name para firstName e lastName, respectivamente
-function formatUser({ id, first_name: firstName, last_name: lastName, email }) {
+function isValid({ id, first_name: firstName, last_name: lastName, email }) {
   // A única coisa que precisamos fazer agora é criar um objeto com os nomes dos campos alterados
   // e sem o campo passowrd
   return {
@@ -17,11 +17,11 @@ function formatUser({ id, first_name: firstName, last_name: lastName, email }) {
 
 // Função responsável por criar o usuário no banco de dados
 function create({ firstName, lastName, email, password }) {
-  const query = 'INSERT INTO users (first_name, last_name, email, password) VALUES (?,?,?,?)';
+  const query = 'INSERT INTO users_crud.users (first_name, last_name, email, password) VALUES (?,?,?,?)';
   // Ao invés de chamarmos connection como uma function, agora utilizamos diretamente o método `execute`
   return connection.execute(query, [firstName, lastName, email, password])
     // Obtemos o resultado da inserção e o utilizamos para obter o ID recém inserido
     .then(([result]) => ({ id: result.insertId, firstName, lastName, email }));
 }
 
-module.exports = { formatUser, create }
+module.exports = { isValid, create }
