3fd17a8bf6ef69e98610e37b04d172731070d166
Seu nome <mgabiss10@gmail.com>
2022-03-29 18:17:43 -0300

    Bloco 23.2 - Realizando exercicio do course  Refatorando index.js para inserir controller

--- modulo_3-Desenvolvimento_Back-End/bloco-23-Node.js:-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/conteudo/hello-msc/index.js
+++ modulo_3-Desenvolvimento_Back-End/bloco-23-Node.js:-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/conteudo/hello-msc/index.js
@@ -1,32 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const Author = require('./services/Authors')
-
 const app = express();
 
+const Author = require('./controllers/Author');
+
 app.use(bodyParser.json());
-app.get('/authors', async (_req, res) => {
-  const authors = await Author.getAll();
-  res.status(200).json(authors);
-});
 
-app.get('/authors/:id', async (req, res) => {
-  const { id } = req.params;
-  const author = await Author.findById(id);
-  if (!author) return res.status(404).json({ message: 'Author not found' });
-  res.status(200).json(author);
-});
-
-app.post('/authors', async (req, res) => {
-  const { first_name, middle_name, last_name } = req.body;
-
-  const author = await Author.createAuthor(first_name, middle_name, last_name);
-
-  if (!author) return res.status(400).json({ message: 'Dados inválidos' });
-
-  res.status(201).json(author);
-});
+app.get('/authors', Author.getAll);
+app.get('/authors/:id', Author.getById);
+app.post('/authors', Author.create);
 
 const PORT = process.env.PORT || 3000;
 
