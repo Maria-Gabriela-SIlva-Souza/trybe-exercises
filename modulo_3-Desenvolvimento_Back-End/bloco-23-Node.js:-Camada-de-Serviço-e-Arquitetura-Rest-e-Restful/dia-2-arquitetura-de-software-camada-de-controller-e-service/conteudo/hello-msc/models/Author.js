8cf932f8a9c2d04d8dc7febab3f4e537d15e4bcd
Seu nome <mgabiss10@gmail.com>
2022-03-29 18:06:12 -0300

    Bloco 23.2 - Realizando exercicio do course - Transferindo funçoes que nao devem estar no model

--- modulo_3-Desenvolvimento_Back-End/bloco-23-Node.js:-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/conteudo/hello-msc/models/Author.js
+++ modulo_3-Desenvolvimento_Back-End/bloco-23-Node.js:-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/conteudo/hello-msc/models/Author.js
@@ -1,21 +1,5 @@
 const connection = require('./connection');
 
-// Cria uma string com o nome completo da pessoa autora
-
-const getNewAuthor = (authorData) => {
-  const { id, firstName, middleName, lastName } = authorData;
-
-  const fullName = [firstName, middleName, lastName].filter((name) => name).join(' ');
-
-  return {
-    id,
-    firstName,
-    middleName,
-    lastName,
-    name: fullName,
-  };
-};
-
 // Converte o nome dos campos de snake_case para camelCase
 const serialize = (authorData) => authorData.map((item) => getNewAuthor({
   id: item.id,
@@ -43,17 +27,7 @@ const findById = async (id) => {
 
   const [authorData] = await connection.execute(query, [id]);
 
-  if (authorData.length === 0) return null;
-
   return serialize(authorData)[0];
-};
-
-const isValid = (firstName, middleName, lastName) => {
-  if (!firstName || typeof firstName !== 'string') return false;
-  if (!lastName || typeof lastName !== 'string') return false;
-  if (middleName && typeof middleName !== 'string') return false;
-
-  return true;
 };
 
 const createAuthor = async (firstName, middleName, lastName) => {
@@ -61,7 +35,7 @@ const createAuthor = async (firstName, middleName, lastName) => {
     'INSERT INTO model_example.authors (first_name, middle_name, last_name) VALUES (?, ?, ?)',
     [firstName, middleName, lastName]
   );
-  return getNewAuthor({ id: author.insertId, firstName, middleName, lastName });
+  return author;
 }
 
 module.exports = {
