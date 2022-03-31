c335f162007debbfc6ceac2095590e4947841809
Seu nome <mgabiss10@gmail.com>
2022-03-31 19:59:07 -0300

    Bloco 23.1 - Exercicio: realizando get /ping

--- modulo_3-Desenvolvimento_Back-End/bloco-23-Node.js:-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exercise/cep-lookup/index.js
+++ modulo_3-Desenvolvimento_Back-End/bloco-23-Node.js:-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exercise/cep-lookup/index.js
@@ -4,4 +4,8 @@ const PORT = 3000;
 
 app.use(express.json());
 
+app.get('/ping', (_req, res) => {
+  res.status(200).json({ "message": "pong!" });
+})
+
 app.listen(PORT, () => console.log(`Exercício de fixação rodando na porta ${PORT}`));
