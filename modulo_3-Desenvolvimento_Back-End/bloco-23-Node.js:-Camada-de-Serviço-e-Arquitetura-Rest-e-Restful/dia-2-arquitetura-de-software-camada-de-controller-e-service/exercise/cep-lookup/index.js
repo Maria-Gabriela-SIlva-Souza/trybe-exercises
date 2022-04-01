1b0d32db00a8c758f525019bebb0885e47a3b92c
Seu nome <mgabiss10@gmail.com>
2022-04-01 18:02:00 -0300

    Bloco 23.2 => Exercício - Criada variável de ambiente, index.js e Get solicitado ao exercício

---
+++ modulo_3-Desenvolvimento_Back-End/bloco-23-Node.js:-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exercise/cep-lookup/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/ping', (_req, res) => {
+  res.status(200).json({ "message": "pong!" });
+})
+
+app.listen(PORT, () => console.log(`Exercício rodando na porta ${PORT}`));
