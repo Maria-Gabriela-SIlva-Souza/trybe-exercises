45d181aa067de43acf6b753bf2a4e25eaf7c5113
Seu nome <mgabiss10@gmail.com>
2022-04-01 12:46:37 -0300

    Retornando exercícios do zero

--- modulo_3-Desenvolvimento_Back-End/bloco-23-Node.js:-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exercise/cep-lookup/controllers/index.js
+++
@@ -1,1 +0,0 @@
-module.exports.pingController = require('./pingController');
--- modulo_3-Desenvolvimento_Back-End/bloco-23-Node.js:-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exercise/cep-lookup/model/connection.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2/promise');
-
-const connection = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: 'gabriela',
-    password: 'maria5287355',
-    database: process.env.MYSQL_DB_NAME });
-
-module.exports = connection;
