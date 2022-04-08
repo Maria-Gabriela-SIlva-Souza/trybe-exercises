7852f3cf3bddf366eb94b6df14e81618191f0ad8
Seu nome <mgabiss10@gmail.com>
2022-04-08 18:47:38 -0300

    Bloco 24.1 ==> Conteudo (seq do zero) - Finalizando CRUD

--- modulo_3-Desenvolvimento_Back-End/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/conteudo/sequelize-do-zero/index.js
+++ modulo_3-Desenvolvimento_Back-End/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/conteudo/sequelize-do-zero/index.js
@@ -39,6 +39,38 @@ app.post('/', (req, res) => {
   })
 })
 
+app.put('/:id', async (req, res) => {
+  try {
+    const { name, description } = req.body;
+    const { id } = req.params;
+
+    const [ updateStore ] = await Store.update(
+      { name, description },
+      { where: { id } }
+    )
+
+    console.log(updateStore)
+
+    if (!updateStore) return res.status(404).json({message: 'Não achamos o usuário !'});
+
+    res.status(200).json({message: 'Usuário cadastrado !'});
+
+
+  } catch {
+    res.status(500).json({message: 'Deu ruim !'});
+  }
+})
+
+app.delete('/:id', (req, res) => {
+  const { id } = req.params
+
+  Store.destroy({ where: { id } }).then(dados => {
+    res.status(201).json({message: 'Usuário deletado !'});
+  }).catch((e) => {
+    res.status(500).json({message: 'Deu ruim !'});
+  })
+})
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
