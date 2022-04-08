02ab512c72d74dd095ad0263a9961f7a246394c3
Seu nome <mgabiss10@gmail.com>
2022-04-08 18:26:05 -0300

    Bloco 24.1 ==> Conteudo (seq do zero) - Inserindo rota get / id

--- modulo_3-Desenvolvimento_Back-End/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/conteudo/sequelize-do-zero/index.js
+++ modulo_3-Desenvolvimento_Back-End/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/conteudo/sequelize-do-zero/index.js
@@ -13,6 +13,16 @@ app.get('/', (req, res) => {
   })
 })
 
+app.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  Store.findByPk(id).then(dados => {
+    res.status(200).json(dados);
+  }).catch((e) => {
+    res.status(500).json({message: 'Deu ruim !'});
+  })
+})
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
