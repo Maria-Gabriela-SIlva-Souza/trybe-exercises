5998a4c12cd60cc52a69f945c986d8f5f7ac4ed4
Seu nome <mgabiss10@gmail.com>
2022-04-08 18:24:29 -0300

    Bloco 24.1 ==> Conteudo (seq do zero) - Inserindo rota get

--- modulo_3-Desenvolvimento_Back-End/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/conteudo/sequelize-do-zero/index.js
+++ modulo_3-Desenvolvimento_Back-End/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/conteudo/sequelize-do-zero/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const { Store } = require('./models');
 
 const app = express();
 
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  Store.findAll().then(dados => {
+    res.status(200).json(dados);
+  }).catch((e) => {
+    res.status(500).json({message: 'Deu ruim !'});
+  })
+})
 
 const PORT = 3000;
 
