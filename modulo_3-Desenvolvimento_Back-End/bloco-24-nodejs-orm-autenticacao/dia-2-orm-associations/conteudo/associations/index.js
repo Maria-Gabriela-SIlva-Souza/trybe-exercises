a2c1227f01509e19335dc6bd2f6bb1c2967691ad
Seu nome <mgabiss10@gmail.com>
2022-04-11 16:27:55 -0300

    Bloco 24.2 ==> Conteudo - Inserindo Managed transactions

--- modulo_3-Desenvolvimento_Back-End/bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/conteudo/associations/index.js
+++ modulo_3-Desenvolvimento_Back-End/bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/conteudo/associations/index.js
@@ -1,17 +1,17 @@
 // index.js
 const express = require('express');
 
-// Criação Unmanaged transactions
+// Criação Unmanaged / Managed transactions
 const Sequelize = require('sequelize');
 
 const { Address, Employee, Book, User } = require('./models');
 
-// Criação Unmanaged transactions
+// Criação Unmanaged / Managed transactions
 const config = require('./config/config');
 
 const app = express();
 
-// Criação Unmanaged transactions
+// Criação Unmanaged / Managed transactions
 const sequelize = new Sequelize(config.development);
 
 
@@ -55,6 +55,7 @@ app.get('/employees/:id', async (req, res) => {
   };
 });
 
+// Criação Unmanaged transactions
 app.post('/employees', async (req, res) => {
   // Primeiro iniciamos a transação
   const t = await sequelize.transaction();
@@ -86,6 +87,37 @@ app.post('/employees', async (req, res) => {
     res.status(500).json({ message: 'Algo deu errado' });
   }
 });
+
+
+// Criação Managed transactions
+app.post('/employees', async (req, res) => {
+  try {
+    const { firstName, lastName, age, city, street, number } = req.body;
+
+    const result = await sequelize.transaction(async (t) => {
+      const employee = await Employee.create({
+        firstName, lastName, age
+      }, { transaction: t });
+
+      await Address.create({
+        city, street, number, employeeId: employee.id
+      }, { transaction: t });
+
+      return res.status(201).json({ message: 'Cadastrado com sucesso' });
+    });
+
+    // Se chegou até aqui é porque as operações foram concluídas com sucesso,
+    // não sendo necessário finalizar a transação manualmente.
+    // `result` terá o resultado da transação, no caso um empregado e o endereço cadastrado
+  } catch (e) {
+    // Se entrou nesse bloco é porque alguma operação falhou.
+    // Nesse caso, o sequelize irá reverter as operações anteriores com a função rollback, não sendo necessário fazer manualmente
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+});
+
+
 
 app.get('/usersbooks/:id', async (req, res) => {
   try {
