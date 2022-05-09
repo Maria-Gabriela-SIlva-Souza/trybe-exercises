daf0da7ff85554fdf79af9e2fb3c36851e71e348
Seu nome <mgabiss10@gmail.com>
2022-05-09 19:26:34 -0300

    Bloco 24.3 - App com token

--- modulo_3-Desenvolvimento_Back-End/bloco-24-nodejs-orm-autenticacao/dia-3-jwt-json-web-token/exercise/hello-jwt/index.js
+++ modulo_3-Desenvolvimento_Back-End/bloco-24-nodejs-orm-autenticacao/dia-3-jwt-json-web-token/exercise/hello-jwt/index.js
@@ -23,6 +23,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/ping', controllers.ping);
 
+app.post('/login', controllers.login);
+
+app.get('/users/me', middlewares.auth, controllers.me);
+
 app.use(middlewares.error);
 
 app.listen(PORT, () => {
