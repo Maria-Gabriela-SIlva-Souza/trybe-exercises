badd6cdc3d36306ddc75004241ed62d851b13360
Seu nome <mgabiss10@gmail.com>
2022-05-19 19:44:34 -0300

    Adicionando api simples

---
+++ modulo_3-Desenvolvimento_Back-End/bloco-25-deployment/dia-1-infraestrutura-deploy-com-heroku/ex-1-ao-3/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (_req, res) => {
+  res.send('Está vivo!!!')
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`))
