f0457643caf3120dd632ca4a36bb2b52132a38a6
Seu nome <mgabiss10@gmail.com>
2022-05-19 19:44:59 -0300

    Bloco 25.1 ==> Adicionando api simples

--- modulo_3-Desenvolvimento_Back-End/bloco-25-deployment/dia-1-infraestrutura-deploy-com-heroku/ex-1-ao-3/index.js
+++ modulo_3-Desenvolvimento_Back-End/bloco-25-deployment/dia-1-infraestrutura-deploy-com-heroku/ex-1-ao-3/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (_req, res) => {
-  res.send('Está vivo!!!')
+  res.send('Está vivo!!')
 });
 
 const PORT = process.env.PORT || 3000;
