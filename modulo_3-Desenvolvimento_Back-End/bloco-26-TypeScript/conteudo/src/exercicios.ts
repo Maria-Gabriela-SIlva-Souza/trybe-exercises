75d9cd3ebda0bfeb53b33983372e6c8897b19c64
Seu nome <mgabiss10@gmail.com>
2022-05-24 21:28:44 -0300

    Bloco 26.1 ==> Conteudo - Exercícios enum

---
+++ modulo_3-Desenvolvimento_Back-End/bloco-26-TypeScript/conteudo/src/exercicios.ts
@@ -0,0 +1,34 @@
+enum diasDaSemana {
+  Segunda = 1,
+  Terça,
+  Quarta,
+  Quinta,
+  Sexta,
+  Sábado,
+  Domingo,
+}
+
+enum coresDoArcoIris {
+  vermelha = "vermelha", 
+  laranja = "laranja", 
+  amarela = "amarela", 
+  verde = "verde", 
+  azul = "azul", 
+  azulEscuro = "azulEscuro",
+  violeta = "violeta"
+}
+
+enum ações {
+  salvar,
+  imprimir,
+  abrir,
+  visualizar, 
+  fechar
+}
+
+enum ações {
+  Norte = "N",
+  Leste = "L", 
+  Sul = "S",
+  Oeste = "O"
+}
