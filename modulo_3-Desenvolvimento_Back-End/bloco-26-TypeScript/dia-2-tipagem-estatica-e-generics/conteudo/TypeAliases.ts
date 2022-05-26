645917f0695f404bf3724a6d76969a87e4e1e5a3
Seu nome <mgabiss10@gmail.com>
2022-05-26 11:12:34 -0300

    Bloco 26.2 ==> Conteudo - Exemplo Type Alyases

---
+++ modulo_3-Desenvolvimento_Back-End/bloco-26-TypeScript/dia-2-tipagem-estatica-e-generics/conteudo/TypeAliases.ts
@@ -0,0 +1,23 @@
+// Crie um type para um objeto que represente um pássaro.
+
+type passaro = {
+  asas: 2;
+  bicos: 1;
+  bípede: true;
+}
+
+
+// Crie um type que represente uma função que recebe 2 valores numéricos e retorna a soma dos dois.
+
+type soma = (x: number, y: number) => number;
+
+
+// Crie um type para um objeto que represente um endereço.
+
+type endereço = {
+  lugarPublico: string;
+  número: number;
+  distrito: string;
+  cidade: string;
+  estado: string;
+}
