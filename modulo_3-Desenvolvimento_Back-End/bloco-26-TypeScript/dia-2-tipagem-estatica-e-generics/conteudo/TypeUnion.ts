b812abac35544720a24d36159589e1bad917bd9a
Seu nome <mgabiss10@gmail.com>
2022-05-26 11:18:32 -0300

    Bloco 26.2 ==> Conteudo - Exemplo Type Union

---
+++ modulo_3-Desenvolvimento_Back-End/bloco-26-TypeScript/dia-2-tipagem-estatica-e-generics/conteudo/TypeUnion.ts
@@ -0,0 +1,15 @@
+// Crie um type union que represente os estados físicos da matéria: líquido, sólido ou gasoso.
+
+type estadosDaMateria = "liquido" | "solido" | "gasoso";
+
+// Crie um type union que represente o documento identificador de uma pessoa que pode receber valores numéricos ou texto. Ex: “123.567.890-12” ou 123456789012.
+
+type documentoInentificador = number | string;
+
+// Crie um type union que represente sistemas operacionais: linux, mac os ou windows.
+
+type sistemasOperacionais = "linux" | "mac" | "windows";
+
+// Crie um type union que represente as vogais do alfabeto.
+
+type vogais = "a" | "e" | "i" | "o" | "u";
