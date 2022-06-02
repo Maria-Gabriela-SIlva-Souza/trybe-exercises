9f90d0ebd72e8ae3e8e20a5a5706318d977f65e6
Seu nome <mgabiss10@gmail.com>
2022-06-01 21:23:50 -0300

    Bloco 27.2 ==> Conteudo - ExFixação Herança - super

--- modulo_3-Desenvolvimento_Back-End/bloco-27-poo-solid/dia-2-heranca-e-interfaces/conteudo/exFixacao.ts
+++ modulo_3-Desenvolvimento_Back-End/bloco-27-poo-solid/dia-2-heranca-e-interfaces/conteudo/exFixacao.ts
@@ -1,3 +1,26 @@
+// class Superclass {
+
+//   constructor(public isSuper = true) { }
+
+//   protected sayHello() {
+//     return  console.log('Olá Mundo')
+//   }
+// }
+
+// class Subclass extends Superclass { 
+//   sayHello2() {
+//     return this.sayHello()
+//   }
+// }
+
+// const myFunc = (object: Subclass) => { return object.sayHello2() }
+
+// // const sup = new Superclass();
+// const sub = new Subclass();
+
+// // myFunc(sup);
+// myFunc(sub);
+
 class Superclass {
 
   constructor(public isSuper = true) { }
@@ -7,9 +30,19 @@ class Superclass {
   }
 }
 
-class Subclass extends Superclass {  }
+class Subclass extends Superclass {
+  // No construtor da _Subclass_, o atributo _isSuper_ deve ser setado como `false`. Você vai precisar utilizar o _super_.
+  constructor() {
+    super();
+    this.isSuper = false;
+  }
+}
 
-const myFunc = (object: Superclass) => { return object.sayHello() }
+const myFunc = (object: Superclass) => { 
+  object.sayHello()
+
+  console.log(object.isSuper ? 'Super!' : 'Sub!');
+}
 
 const sup = new Superclass();
 const sub = new Subclass();
