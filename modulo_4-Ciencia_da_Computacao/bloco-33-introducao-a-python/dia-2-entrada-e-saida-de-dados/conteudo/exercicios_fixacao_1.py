b96cf3cf717693bd27957acc2463ad8a87971002
Seu nome <mgabiss10@gmail.com>
2022-08-01 21:10:29 -0300

    Bloco 33.2 ==> Conteudo - exFixacao 1 e 2

---
+++ modulo_4-Ciencia_da_Computacao/bloco-33-introducao-a-python/dia-2-entrada-e-saida-de-dados/conteudo/exercicios_fixacao_1.py
@@ -0,0 +1,9 @@
+# Exercício 1:  Faça um programa que solicite o nome de
+#               uma pessoa usuária e imprima-o na vertical.
+
+
+NOME = input('Digite seu nome:')
+
+
+for letra in NOME:
+    print(letra)
