5c6ed09ace66c8a7ced294654a9da42484791afc
Seu nome <mgabiss10@gmail.com>
2022-08-01 21:28:59 -0300

    Bloco 33.2 ==> Conteudo - exemplo course leitura e escrita de arquivos

---
+++ modulo_4-Ciencia_da_Computacao/bloco-33-introducao-a-python/dia-2-entrada-e-saida-de-dados/conteudo/personagens_legais.py
@@ -0,0 +1,15 @@
+characters_file = open('meus_personagens_favoritos.txt', mode='w')
+
+
+characters_file.write('Tio Patinhas\n')
+characters_file.write('Neo\n')
+characters_file.write('Homem Aranha\n')
+characters_file.write('Harry Potter\n')
+
+print('Batman', file=characters_file)
+
+more_characters = ['Hrmione\n', 'Coringa\n']
+
+characters_file.writelines(more_characters)
+
+characters_file.close()
