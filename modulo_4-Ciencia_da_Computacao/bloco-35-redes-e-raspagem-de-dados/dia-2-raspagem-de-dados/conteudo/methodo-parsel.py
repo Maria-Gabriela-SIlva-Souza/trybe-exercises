d21abae05138f13afc17258a3be4866ab9dd6586
Seu nome <mgabiss10@gmail.com>
2022-09-20 22:15:49 -0300

    Bloco 35.2 - Exemplos parsel

--- modulo_4-Ciencia_da_Computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-raspagem-de-dados/conteudo/methodo-parsel.py
+++ modulo_4-Ciencia_da_Computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-raspagem-de-dados/conteudo/methodo-parsel.py
@@ -1,13 +1,23 @@
-import parsel
+from parsel import Selector
 import requests
 
 
+# Retorno de todo o html da página
 response = requests.get("http://books.toscrape.com/")
-# print(response.text)
+selector = Selector(text=response.text)
+# print(selector)
 
+# Retorno de todos os títulos
+titles = selector.css(".product_pod h3 a::attr(title)").getall()
+# print(titles)
 
-selector = parsel.Selector(text=response.text)
-selectorPrint = selector.css('img.thumbnail').getall()
+# Retorno de todos os preços
+prices = selector.css(".product_price .price_color::text").getall()
+# print(prices)
 
-for thumbnail in selectorPrint:
-    print(thumbnail)
+# Combinando tudo podemos buscar os produtos
+# em em seguida buscar os valores individualmente
+for product in selector.css(".product_pod"):
+    title = product.css("h3 a::attr(title)").get()
+    price = product.css(".price_color::text").get()
+    print(title, price)
