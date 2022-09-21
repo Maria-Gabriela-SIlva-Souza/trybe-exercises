2d748c5470b0e6a914bb2054889159f3a253cf8e
Seu nome <mgabiss10@gmail.com>
2022-09-20 22:30:11 -0300

    Bloco 35.2 - Exemplos pegar paginação

--- modulo_4-Ciencia_da_Computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-raspagem-de-dados/conteudo/methodo-parsel.py
+++ modulo_4-Ciencia_da_Computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-raspagem-de-dados/conteudo/methodo-parsel.py
@@ -17,7 +17,14 @@ prices = selector.css(".product_price .price_color::text").getall()
 
 # Combinando tudo podemos buscar os produtos
 # em em seguida buscar os valores individualmente
-for product in selector.css(".product_pod"):
-    title = product.css("h3 a::attr(title)").get()
-    price = product.css(".price_color::text").get()
-    print(title, price)
+# for product in selector.css(".product_pod"):
+#     title = product.css("h3 a::attr(title)").get()
+#     price = product.css(".price_color::text").get()
+#     print(title, price)
+
+# Existe uma classe next, que podemos recuperar
+#  a url através do seu elemento âncora
+# Não esquecendo que temos a parte raiz do site
+# http://books.toscrape.com/
+next_page_url = selector.css(".next a::attr(href)").get()
+print(next_page_url)
